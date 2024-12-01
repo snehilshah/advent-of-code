ad487e5c9659529a994642b569880f75ac1482e7
snehilshah <snehilshah27@gmail.com>
2024-12-02 01:51:48 +0530

    :boom: Big :exclamation:

---
+++ 2024/golang/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World, This is my advent of code 2024 repo golang repo")
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Advent of Code Archive
+
+Find all my advent of code solutions here
+
+- [x] - golang
+- [ ] - rust
